b1b87881b6454203fa99624baf8c542083ac07cc
Hansamemiya <122122358+Hansamemiya@users.noreply.github.com>
2025-02-21 21:09:19 +0000

    Finish

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,53 +1,61 @@
 class CoursesController < ApplicationController
   def index
     @courses = Course.all.order({ :created_at => :desc })
-
-    render({ :template => "courses/index" })
+    render({ template: "courses/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-    @course = Course.where({:id => the_id }).at(0)
+    @course = Course.where({ :id => the_id }).at(0)
 
-    render({ :template => "courses/show" })
+    if @course
+      render({ template: "courses/show" })
+    else
+      redirect_to("/courses", { alert: "Course not found." })
+    end
   end
 
   def create
     @course = Course.new
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
-    @course.department_id = params.fetch("query_department_id")
+    @course.title = params.fetch("query_title", "").strip
+    @course.term_offered = params.fetch("query_term_offered", "").strip
+    @course.department_id = params.fetch("query_department_id", "").strip
 
-    if @course.valid?
-      @course.save
-      redirect_to("/courses", { :notice => "Course created successfully." })
+    if @course.save
+      redirect_to("/courses", { notice: "Course created successfully." })
     else
-      redirect_to("/courses", { :notice => "Course failed to create successfully." })
+      redirect_to("/courses", { alert: "Course creation failed: " + @course.errors.full_messages.join(", ") })
     end
   end
 
   def update
-    id = params.fetch("path_id")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_offered")
-    @course.department_id = params.fetch("query_department_id")
+    if @course
+      @course.title = params.fetch("query_title", @course.title).strip
+      @course.term_offered = params.fetch("query_term_offered", @course.term_offered).strip
+      @course.department_id = params.fetch("query_department_id", @course.department_id).strip
 
-    if @course.valid?
-      @course.save
-      redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully."} )
+      if @course.save
+        redirect_to("/courses/#{@course.id}", { notice: "Course updated successfully." })
+      else
+        redirect_to("/courses/#{@course.id}", { alert: "Course update failed: " + @course.errors.full_messages.join(", ") })
+      end
     else
-      redirect_to("/courses/#{@course.id}", { :alert => "Course failed to update successfully." })
+      redirect_to("/courses", { alert: "Course not found." })
     end
   end
 
   def destroy
-    the_id = params.fetch("path")
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
-    @course.destroy
-
-    redirect_to("/courses", { :notice => "Course deleted successfully."} )
+    if @course
+      @course.destroy
+      redirect_to("/courses", { notice: "Course deleted successfully." })
+    else
+      redirect_to("/courses", { alert: "Course not found." })
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,32 @@
 Rails.application.routes.draw do
+  # Root route
   get("/", { :controller => "departments", :action => "index" })
 
-  #------------------------------
+  # ------------------------------
   # Department routes
-
-  get("/departments", { :controller => "department", :action => "index" })
-  
-  get("/departments/:path_id", { :controller => "departments", :action => "show" })
-
-  post("/insert_department", { :controller => "departments", :action => "create" })
-
   get("/departments", { :controller => "departments", :action => "index" })
-  
-  post("/modify_department/:path_id", { :controller => "departments", :action => "update" })
+  get("/departments/:path_id", { :controller => "departments", :action => "show" })
+  post("/insert_department", { :controller => "departments", :action => "create" })
+  patch("/modify_department/:path_id", { :controller => "departments", :action => "update" })
+  delete("/delete_department/:path_id", { :controller => "departments", :action => "destroy" })
 
-  get("/delete_department/:path_id", { :controller => "departments", :action => "destroy" })
-
-  #------------------------------
+  # ------------------------------
   # Course routes
-
   get("/courses", { :controller => "courses", :action => "index" })
-  
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
-
-  get("/insert_course", { :controller => "courses", :action => "create" })
-
-  get("/courses", { :controller => "courses_controller", :action => "index" })
-  
-  get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
-
+  post("/insert_course", { :controller => "courses", :action => "create" })
+  patch("/modify_course/:path_id", { :controller => "courses", :action => "update" })
   get("/delete_course/:path_id", { :controller => "courses", :action => "destroy" })
 
-  #------------------------------
+  # ------------------------------
   # Student routes
-
   get("/students", { :controller => "students", :action => "index" })
-  
   get("/students/:path_id", { :controller => "students", :action => "show" })
-
   post("/insert_student", { :controller => "students", :action => "create" })
+  patch("/modify_student/:path_id", { :controller => "students", :action => "update" })
+  delete("/delete_student/:path_id", { :controller => "students", :action => "destroy" })
 
-  get("/students", { :controller => "students", :action => "index" })
-  
-  post("/modify_student/:path_id", { :controller => "students", :action => "update" })
-
-  get("/delete_student/:path_id", { :controller => "students", :action => "destroy" })
-
-  #------------------------------
-
+  # ------------------------------
+  # Enrollment routes
   post("/insert_enrollment", { :controller => "enrollments", :action => "create" })
-
 end
